9bdcd7f68e9db1207514fc033dfa2fe4b6ed43e7
Smit Gandhi <smitgandhi585@gmail.com>
2025-07-03 15:53:37 +0530

    Added env and evaluation pipeline

--- src/DS_project/pipeline/model_evaluation_pipeline.py
+++ src/DS_project/pipeline/model_evaluation_pipeline.py
@@ -1,9 +1,14 @@
 from src.DS_project.components.model_evaluation import Model_evaluation
 from src.DS_project.config.configuration import ConfigurationManager
 import os
-os.environ["MLFLOW_TRACKING_URI"] = "https://dagshub.com/smitngandhi/DS_project.mlflow"
-os.environ["MLFLOW_TRACKING_USERNAME"] = "smitngandhi"
-os.environ["MLFLOW_TRACKING_PASSWORD"] = "cead263a2fe84d23cdc7b91d33875c47d16c3e5b"
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+os.environ["MLFLOW_TRACKING_URI"] = os.getenv("MLFLOW_TRACKING_URI")
+os.environ["MLFLOW_TRACKING_USERNAME"] = os.getenv("MLFLOW_TRACKING_USERNAME")
+os.environ["MLFLOW_TRACKING_PASSWORD"] = os.getenv("MLFLOW_TRACKING_PASSWORD")
 
 class ModelEvaluationPipeline:
     def __init__(self):
